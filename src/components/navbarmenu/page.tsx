a7dd1daa20287fb4158cab245420031c9b445bb3
Aerley <lopezkkler65@gmail.com>
2024-11-16 13:28:15 -0600

    commit de confirmacion

--- src/components/navbarmenu/page.tsx
+++ src/components/navbarmenu/page.tsx
@@ -25,8 +25,8 @@ import {
 
 function NavbarMenu() {
   return (
-    <nav className="flex flex-col h-fit items-center justify-between bg-[#1b1b1b] text-white w-full py-4 m-auto z-10">
-      <section className="flex items-center justify-between w-full lg:w-[1900px] md:px-24">
+    <nav className="flex flex-col h-fit items-center justify-between fixed bg-[#1b1b1bd3] text-white w-full py-4 m-auto z-10">
+      <section className="flex items-center justify-between w-full 2xl:w-[1900px] md:px-24">
         <div className="flex items-center justify-center gap-8">
           <Image priority alt="Tecpoint Logo" src="/logo.png" width={180} height={80} className="aspect-[180-80]" />
         </div>
