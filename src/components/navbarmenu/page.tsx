44553570215a42dde4265c7595c81f9ea0f4fcbc
Aerley <tecpointdistribucion2@gmail.com>
2024-11-21 22:16:17 -0600

    agregando NavigationMenu

--- src/components/navbarmenu/page.tsx
+++ src/components/navbarmenu/page.tsx
@@ -24,6 +24,19 @@ import {
   SheetTrigger,
 } from "@/components/ui/sheet";
 
+import * as React from "react"
+
+import { cn } from "@/lib/utils"
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+  navigationMenuTriggerStyle,
+} from "@/components/ui/navigation-menu"
+
 function NavbarMenu() {
   return (
     <nav className="flex flex-col h-fit items-center px-2 justify-between bg-[#1b1b1b] backdrop-blur text-white w-full py-4 m-auto z-[999]">
@@ -45,11 +58,49 @@ function NavbarMenu() {
           <Link href="/" className="text-[14px] font-[500] font-[Poppins]">
             Inicio
           </Link>
+
+          <NavigationMenu>
+            <NavigationMenuList>
+              <NavigationMenuItem>
+                <NavigationMenuTrigger>Categorias</NavigationMenuTrigger>
+                <NavigationMenuContent>
+                  <ul className="grid gap-3 p-4 md:w-[400px] lg:w-[500px] lg:grid-cols-[.75fr_1fr]">
+                    <li className="row-span-3">
+                      <NavigationMenuLink asChild>
+                        <a
+                          className="flex h-full w-full select-none flex-col justify-end rounded-md bg-gradient-to-b from-muted/50 to-muted p-6 no-underline outline-none focus:shadow-md"
+                          href="/"
+                        >
+                          <div className="mb-2 mt-4 text-lg font-medium">
+                            shadcn/ui
+                          </div>
+                          <p className="text-sm leading-tight text-muted-foreground">
+                            Beautifully designed components built with Radix UI and
+                            Tailwind CSS.
+                          </p>
+                        </a>
+                      </NavigationMenuLink>
+                    </li>
+                    <ListItem href="/docs" title="Introduction">
+                      Re-usable components built using Radix UI and Tailwind CSS.
+                    </ListItem>
+                    <ListItem href="/docs/installation" title="Installation">
+                      How to install dependencies and structure your app.
+                    </ListItem>
+                    <ListItem href="/docs/primitives/typography" title="Typography">
+                      Styles for headings, paragraphs, lists...etc
+                    </ListItem>
+                  </ul>
+                </NavigationMenuContent>
+              </NavigationMenuItem>
+            </NavigationMenuList>
+          </NavigationMenu>
+
           <Link href="/" className="text-[14px] font-[500] font-[Poppins]">
-            Categorías
+            Blog
           </Link>
           <Link href="/shop" className="text-[14px] font-[500] font-[Poppins]">
-            Productos
+            Tienda
           </Link>
         </div>
 
@@ -170,3 +221,29 @@ function NavbarMenu() {
 }
 
 export default NavbarMenu;
+
+const ListItem = React.forwardRef<
+  React.ElementRef<"a">,
+  React.ComponentPropsWithoutRef<"a">
+>(({ className, title, children, ...props }, ref) => {
+  return (
+    <li>
+      <NavigationMenuLink asChild>
+        <a
+          ref={ref}
+          className={cn(
+            "block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground",
+            className
+          )}
+          {...props}
+        >
+          <div className="text-sm font-medium leading-none">{title}</div>
+          <p className="line-clamp-2 text-sm leading-snug text-muted-foreground">
+            {children}
+          </p>
+        </a>
+      </NavigationMenuLink>
+    </li>
+  )
+})
+ListItem.displayName = "ListItem"
--- src/pages/shop/[slug].tsx
+++ src/pages/shop/[slug].tsx
@@ -16,7 +16,7 @@ interface ProductDetailProps {
 export const getStaticPaths: GetStaticPaths = async () => {
   try {
     const querySnapshot = await getDocs(collection(db, process.env.NEXT_PUBLIC_DATABASE_NAME as string));
-    
+
     const paths = querySnapshot.docs.map((doc) => {
       const data = doc.data();
       if (data.slug) {
@@ -149,7 +149,7 @@ const ProductDetail = ({ product }: ProductDetailProps) => {
           <CarouselNext />
         </Carousel>
 
-        <div className="w-[45%]">
+        <div className="w-[40%]">
           <div className="flex flex-col gap-y-3">
             <Image
               quality={96}
@@ -193,10 +193,13 @@ const ProductDetail = ({ product }: ProductDetailProps) => {
 
           <button
             onClick={handleAddToCart}
-            className={`mt-6 px-8 py-3 text-black font-semibold rounded-md ${
-              isAddedToCart ? "bg-transparent border-[1.4px] border-black" : "bg-black text-black hover:bg-transparent border-black border-[1.4px] hover:text-black transition-colors"
-            }`}
+            className={`flex gap-x-3 mt-6 px-12 items-center justify-center py-3 text-black rounded-sm ${isAddedToCart ? "bg-transparent border-[1.4px] border-black text-black" : "bg-black text-white hover:bg-transparent border-black border-[1.4px] hover:text-black transition-colors"
+              }`}
           >
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+              <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z" />
+            </svg>
+
             {isAddedToCart ? "Producto en el carrito" : "Agregar al carrito"}
           </button>
         </div>
