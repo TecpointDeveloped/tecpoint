5ae6170ff8eef05d7276a2f532577742d137dae1
Aerley <tecpointdistribucion2@gmail.com>
2024-12-15 15:43:53 -0600

    new desing login page

--- src/pages/my-account.tsx
+++ src/pages/my-account.tsx
@@ -5,6 +5,7 @@ import Head from "next/head";
 import Image from "next/image";
 import { useRouter } from "next/router";
 import { useState } from "react";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
 
 function My_Acoount() {
   const { signInWithGoogle, signInWithEmailAndPassword } = useAuth();
@@ -46,69 +47,129 @@ function My_Acoount() {
   };
 
   return (
-    <main className="flex bg-[#f7f7f7] w-full h-screen">
+    <main className="bg-[#f7f7f7] h-screen p-4 flex justify-center items-start">
       <Head>
         <title>Incio de Sesion | Tecpoint</title>
       </Head>
 
-      <section className="h-full p-2 flex flex-col items-center justify-center gap-8">
-        <div className="2xl:p-8 flex flex-col gap-4 justify-center items-center">
-          <h1 className="font-[Poppins] font-[600] md:text-3xl 2xl:text-3xl text-center tracking-[-1.1px]">Bienvenido de Nuevo!</h1>
-          <h2 className="text-[15px]">Iniciar sesión con los siguientes proveedores</h2>
-        </div>
+      <Tabs defaultValue="account" className="w-[400px] h-fit flex flex-col items-center">
+        <TabsList className="bg-[#eeeeee] w-fit h-fit">
+          <TabsTrigger className="py-[10px] px-12" value="account">Iniciar sesion</TabsTrigger>
+          <TabsTrigger className="py-[10px] px-12" value="password">Eres Mayorista?</TabsTrigger>
+        </TabsList>
 
-        <div className="flex flex-col gap-y-2 w-full items-center">
-          <button
-            onClick={signInWithGoogle}
-            className="hover:bg-gray-200 transition-colors text-black font-[Poppins] w-[290px] h-12 rounded-[8px] bg-white flex items-center justify-start gap-2 px-8"
-          >
-            <Image alt="google icons" src="/google.svg" height={20} width={20} />
-            Iniciar con Google
-          </button>
+        <TabsContent className="pt-4" value="account">
+          <section className="flex flex-col items-center justify-center gap-8">
+            {/* <div className="bg-black p-1 rounded-xl size-fit">
+              <Image alt="logo tecpoint" src="/favicon.ico" height={50} width={50} />
+            </div> */}
 
-          <button className="hover:bg-gray-200 transition-colors text-black font-[Poppins] w-[290px] h-12 rounded-[8px] bg-white flex items-center justify-start gap-2 px-8">
-            <Image alt="facebook icons" src="/facebook.svg" height={20} width={20} />
-            Iniciar con Facebook
-          </button>
-        </div>
-
-        <form onSubmit={handleLogin} className="flex flex-col p-4 gap-y-3 items-center justify-center w-full">
-          <input
-            className={`text-[15px] px-4 py-2 w-[290px] h-12 rounded-[8px] text-black ${emailError ? 'border border-red-500' : ''}`}
-            id="email"
-            name="email"
-            type="email"
-            placeholder="Ingrese su Correo"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-          <input
-            className={`text-[15px] px-4 py-2 w-[290px] h-12 rounded-[8px] text-black ${passwordError ? 'border border-red-500' : ''}`}
-            id="password"
-            name="password"
-            type="password"
-            placeholder="Ingrese su Contraseña"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-
-          <button className="bg-black text-white relative px-4 py-2 w-[290px] h-12 rounded-[8px] flex items-center justify-center gap-x-4">
-
-            <div role="status" className="absolute left-14">
-              <svg aria-hidden="true" className="w-5 h-5 text-[#4e4e4e] animate-spin fill-white" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor" />
-                <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill" />
-              </svg>
+            <div className="flex flex-col justify-center items-center">
+              <h1 className="font-[Poppins] font-[600] md:text-3xl 2xl:text-3xl text-center tracking-[-1.1px]">Bienvenido de Nuevo!</h1>
+              <h2 className="text-[15px]">Iniciar sesión con los siguientes proveedores</h2>
             </div>
 
-            Iniciar Sesión
-          </button>
-        </form>
+            <div className="flex flex-col gap-y-2 w-full items-center">
+              <button
+                onClick={signInWithGoogle}
+                className="hover:bg-gray-200 transition-colors text-black font-[Poppins] w-[290px] h-12 rounded-[8px] bg-white flex items-center justify-start gap-2 px-8"
+              >
+                <Image alt="google icons" src="/google.svg" height={20} width={20} />
+                Iniciar con Google
+              </button>
 
-        <span className="h-6">
-          {error && <div className="text-red-500">{error}</div>}
-        </span>
-      </section>
+              <button className="hover:bg-gray-200 transition-colors text-black font-[Poppins] w-[290px] h-12 rounded-[8px] bg-white flex items-center justify-start gap-2 px-8">
+                <Image alt="facebook icons" src="/facebook.svg" height={20} width={20} />
+                Iniciar con Facebook
+              </button>
+            </div>
+
+            <form onSubmit={handleLogin} className="flex flex-col p-4 gap-y-3 items-center justify-center w-full">
+              <input
+                className={`text-[15px] px-4 py-2 w-[290px] h-12 rounded-[8px] text-black ${emailError ? 'border border-red-500' : ''}`}
+                id="email"
+                name="email"
+                type="email"
+                placeholder="Ingrese su Correo"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+              <input
+                className={`text-[15px] px-4 py-2 w-[290px] h-12 rounded-[8px] text-black ${passwordError ? 'border border-red-500' : ''}`}
+                id="password"
+                name="password"
+                type="password"
+                placeholder="Ingrese su Contraseña"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+
+              <button className="bg-black text-white relative px-4 py-2 w-[290px] h-12 rounded-[8px] flex items-center justify-center gap-x-4">
+
+                <div role="status" className="absolute left-14">
+                  <svg aria-hidden="true" className="w-5 h-5 text-[#4e4e4e] animate-spin fill-white" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor" />
+                    <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill" />
+                  </svg>
+                </div>
+
+                Iniciar Sesión
+              </button>
+            </form>
+
+            <span className="h-6">
+              {error && <div className="text-red-500">{error}</div>}
+            </span>
+          </section>
+        </TabsContent>
+
+        <TabsContent className="pt-4" value="password">
+          <section className="flex flex-col gap-8 items-center">
+            {/* <div className="bg-black p-1 rounded-xl size-fit">
+              <Image alt="logo tecpoint" src="/favicon.ico" height={50} width={50} />
+            </div> */}
+
+            <div className="flex flex-col gap-y-4">
+              <h1 className="font-[Poppins] font-[600] md:text-3xl 2xl:text-3xl text-center tracking-[-1.1px]">Cuentas Mayoristas tecpoint</h1>
+
+              <form onSubmit={handleLogin} className="flex flex-col p-4 gap-y-3 items-center justify-center w-full">
+                <input
+                  className={`text-[15px] px-4 py-2 w-[290px] h-12 rounded-[8px] text-black ${emailError ? 'border border-red-500' : ''}`}
+                  id="email"
+                  name="email"
+                  type="email"
+                  placeholder="Ingrese su correo mayorista"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+
+                <input
+                  className={`text-[15px] px-4 py-2 w-[290px] h-12 rounded-[8px] text-black ${passwordError ? 'border border-red-500' : ''}`}
+                  id="password"
+                  name="password"
+                  type="password"
+                  placeholder="Ingrese su contraseña"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+
+                <button className="bg-black text-white relative px-4 py-2 w-[290px] h-12 rounded-[8px] flex items-center justify-center gap-x-4">
+
+                  <div role="status" className="absolute left-14">
+                    <svg aria-hidden="true" className="w-5 h-5 text-[#4e4e4e] animate-spin fill-white" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+                      <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor" />
+                      <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill" />
+                    </svg>
+                  </div>
+
+                  Iniciar Sesión
+                </button>
+              </form>
+            </div>
+          </section>
+        </TabsContent>
+      </Tabs>
+
     </main>
   );
 }
