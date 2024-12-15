e74a304fefe6f863e858d8434aa88b9504e77cc5
Aerley <tecpointdistribucion2@gmail.com>
2024-12-15 15:54:05 -0600

    Mejorando SEO

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -20,8 +20,8 @@ export default function Document() {
 
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:title" content="Distribuidores de Accesorios Tecnológicos | Tecpoint" />
-        <meta name="twitter:description" content="Distribuidores de Accesorios Tecnológicos | Tecpoint" />
-        <meta name="twitter:image" content="favicon.png" />
+        <meta name="twitter:description" content="Distribuidor de accesorios tecnológicos en Honduras. Cargadores, adaptadores, audífonos, periféricos y más, al por mayor y al detalle." />
+        <meta name="twitter:image" content="/favicon.png" />
         <meta name="twitter:image:alt" content="Distribuidores de Accesorios Tecnológicos | Tecpoint" />
       </Head>
       <body className="antialiased">
