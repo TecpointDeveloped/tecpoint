0b1d17ca573eb6fa7d8df27d0c38a62ec6c0a218
Aerley <tecpointdistribucion2@gmail.com>
2024-12-23 22:10:46 -0600

    solucion de error unknown

--- src/pages/api/checkout/route.ts
+++ src/pages/api/checkout/route.ts
@@ -30,9 +30,9 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     const response = await paypalClient.execute(request);
 
     return res.status(200).json({ details: response.result });
-  } catch (error) {
+  } catch (error: any) {
     console.error("Error al capturar el pedido:", error);
-    
+
     return res.status(500).json({ error: "Error al capturar el pedido", details: error.message });
   }
 }
