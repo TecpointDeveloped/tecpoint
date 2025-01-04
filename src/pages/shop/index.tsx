ad76457270f58d5a5cc20c01c2d0ee2bc2e1f4e9
Aerley <tecpointdistribucion2@gmail.com>
2025-01-04 11:38:58 -0600

    paginacion agregada

--- src/pages/shop/index.tsx
+++ src/pages/shop/index.tsx
@@ -7,16 +7,19 @@ import { Product } from "@/types/ProductTypes";
 import { collection, getDocs } from "firebase/firestore";
 import NavbarMenu from "@/components/navbarmenu/page";
 import Footer from "@/components/Footer/page";
+import { Pagination, PaginationContent, PaginationItem, PaginationLink, PaginationNext, PaginationPrevious, } from "@/components/ui/pagination";
 
 interface ShopProps {
   products: Product[];
+  totalProducts: number;
 }
 
 export async function getStaticProps() {
   try {
-    const querySnapshot = await getDocs(
-      collection(db, process.env.NEXT_PUBLIC_DATABASE_NAME as string)
-    );
+    const productsRef = collection(db, process.env.NEXT_PUBLIC_DATABASE_NAME as string);
+    const querySnapshot = await getDocs(productsRef);
+
+    const totalProducts = querySnapshot.size;
     const products = querySnapshot.docs.map((doc) => {
       const data = doc.data();
       return {
@@ -29,6 +32,7 @@ export async function getStaticProps() {
     return {
       props: {
         products,
+        totalProducts,
       },
       revalidate: 60,
     };
@@ -37,6 +41,7 @@ export async function getStaticProps() {
     return {
       props: {
         products: [],
+        totalProducts: 0,
       },
     };
   }
@@ -44,6 +49,8 @@ export async function getStaticProps() {
 
 const Shop = ({ products = [] }: ShopProps) => {
   const [searchTerm, setSearchTerm] = useState("");
+  const [currentPage, setCurrentPage] = useState(1);
+  const productsPerPage = 9;
 
   const filteredProducts = products.filter((product) =>
     product.producto.toLowerCase().includes(searchTerm.toLowerCase()) ||
@@ -51,6 +58,16 @@ const Shop = ({ products = [] }: ShopProps) => {
     product.descripcion.toUpperCase().includes(searchTerm.toUpperCase()) ||
     product.marca_producto.marca.toLowerCase().includes(searchTerm.toLowerCase())
   );
+
+  const indexOfLastProduct = currentPage * productsPerPage;
+  const indexOfFirstProduct = indexOfLastProduct - productsPerPage;
+  const currentProducts = filteredProducts.slice(indexOfFirstProduct, indexOfLastProduct);
+
+  const totalPages = Math.ceil(filteredProducts.length / productsPerPage);
+
+  const handlePageChange = (page: number) => {
+    setCurrentPage(page);
+  };
 
   if (!products || products.length === 0) {
     return (
@@ -83,14 +100,9 @@ const Shop = ({ products = [] }: ShopProps) => {
       </Head>
 
       <main className="w-full mx-auto p-2 md:p-4 mt-12">
-        <h1 className="text-2xl font-bold mb-6 text-center">
-          Bienvenido a la tienda Tecpoint
-        </h1>
+        <h1 className="text-2xl font-bold mb-6 text-center">Bienvenido a la tienda Tecpoint</h1>
 
-        <form
-          onSubmit={(e) => e.preventDefault()}
-          className="w-full md:py-8 md:px-12 mb-2"
-        >
+        <form onSubmit={(e) => e.preventDefault()} className="w-full md:py-8 md:px-12 mb-2">
           <input
             className="border w-full py-3 px-6 rounded-full"
             type="text"
@@ -100,9 +112,39 @@ const Shop = ({ products = [] }: ShopProps) => {
           />
         </form>
 
+        {/* Paginación */}
+        <div className="flex gap-3 mb-8">
+          <Pagination className="flex gap-4">
+            <PaginationPrevious
+              onClick={() => currentPage > 1 && handlePageChange(currentPage - 1)}
+              className="cursor-pointer select-none"
+            >
+              Anterior
+            </PaginationPrevious>
+            <PaginationContent>
+              {Array.from({ length: totalPages }, (_, index) => index + 1).map((page) => (
+                <PaginationItem key={page}>
+                  <PaginationLink
+                    onClick={() => handlePageChange(page)}
+                    isActive={currentPage === page}
+                    className="cursor-pointer select-none"
+                  >
+                    {page}
+                  </PaginationLink>
+                </PaginationItem>
+              ))}
+            </PaginationContent>
+            <PaginationNext
+              onClick={() => currentPage < totalPages && handlePageChange(currentPage + 1)}
+              className="cursor-pointer select-none"
+            >
+              Siguiente
+            </PaginationNext>
+          </Pagination>
+        </div>
 
         <div className="grid grid-cols-2 md:grid-cols-3 gap-4 md:w-[1100px] mx-auto">
-          {filteredProducts.map((product: Product) => {
+          {currentProducts.map((product: Product) => {
             const imagen_01 = product.imagenes?.imagen_01?.img || "/default-product.png";
 
             return (
@@ -111,19 +153,10 @@ const Shop = ({ products = [] }: ShopProps) => {
                 className="border rounded-[26px] p-4 flex flex-col w-[190px] sm:w-[300px] md:w-[340px] md:h-[450px] relative justify-between"
               >
                 <div className="flex flex-col">
-                  <Link
-                    href={`/shop/${product.slug}`}
-                    className="hover:scale-105 transition-transform"
-                    rel="noopener noreferrer"
-                    download={false}
-                  >
+                  <Link href={`/shop/${product.slug}`} className="hover:scale-105 transition-transform" rel="noopener noreferrer" download={false}>
                     <Image
                       src={imagen_01}
-                      alt={
-                        product.producto
-                          ? `Imagen de ${product.producto}`
-                          : "Imagen del producto"
-                      }
+                      alt={product.producto ? `Imagen de ${product.producto}` : "Imagen del producto"}
                       width={240}
                       height={240}
                       className="m-auto sm:size-[240px] size-[180px] aspect-square object-cover mb-4"
@@ -133,19 +166,12 @@ const Shop = ({ products = [] }: ShopProps) => {
                   </Link>
 
                   <div>
-                    <h2 className="text-[13px] md:text-[17px] font-semibold tracking-[-0.2px] leading-[18px]">
-                      {product.producto}
-                    </h2>
-                    <p className="text-sm text-gray-500 mt-2">
-                      SKU: {product.sku}
-                    </p>
+                    <h2 className="text-[13px] md:text-[17px] font-semibold tracking-[-0.2px] leading-[18px]">{product.producto}</h2>
+                    <p className="text-sm text-gray-500 mt-2">SKU: {product.sku}</p>
 
                     <div className="flex flex-wrap mt-4 gap-2 overflow-hidden w-full h-[26px]">
                       {(product.categorias || []).map((cat: string, index: number) => (
-                        <span
-                          key={index}
-                          className="bg-gray-200 text-gray-700 text-xs font-semibold px-2 py-1 rounded w-fit h-fit"
-                        >
+                        <span key={index} className="bg-gray-200 text-gray-700 text-xs font-semibold px-2 py-1 rounded w-fit h-fit">
                           {cat}
                         </span>
                       ))}
@@ -154,14 +180,13 @@ const Shop = ({ products = [] }: ShopProps) => {
                 </div>
 
                 <button className="mt-4 w-full bg-black text-white py-2 px-4 rounded-full hover:bg-black/80">
-                  <Link href={`/shop/${product.slug}`}>
-                    Ver Producto
-                  </Link>
+                  <Link href={`/shop/${product.slug}`}>Ver Producto</Link>
                 </button>
               </div>
             );
           })}
         </div>
+
       </main>
 
       <Footer />
