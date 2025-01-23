7aa2436396fe3e6e616402dab6eaf6d2aa6be8b7
Aerley <tecpointdistribucion2@gmail.com>
2025-01-23 16:48:51 -0600

    ui mejorada

--- src/pages/cart/index.tsx
+++ src/pages/cart/index.tsx
@@ -44,74 +44,74 @@ const CartPage = () => {
   };
 
   // Calcular el total de la compra
-  const getTotal = () => {
-    return cart.reduce((total, item) => total + (item.precio || 0) * item.quantity, 0)
-  };
+  // const getTotal = () => {
+  //   return cart.reduce((total, item) => total + (item.precio || 0) * item.quantity, 0)
+  // };
 
-  const handlePayment = async () => {
-    const orderData = {
-      settings: {
-        endpoint: "https://pixel-pay.com",
-        credentials: {
-          username: "FHI372717363",
-          password: "b36aa20b0010f042b5bf788a4793b902",
-        },
-      },
-      card: {
-        number: "4111111111111111",
-        cvv2: "2512",
-        expire_month: 9,
-        expire_year: 2027,
-        cardholder: "Aerley Keller Lopez Ramos",
-      },
-      billing: {
-        address: "9 avenida",
-        country: "HN",
-        state: "HN-CR",
-        city: "San Pedro Sula",
-        phone: "98007330",
-      },
-      order: {
-        id: `ORDER-${Date.now()}`,
-        currency: "HNL",
-        customer_name: "Aerley Lopez",
-        customer_email: "example@gmail.com",
-        items: cart.map((item) => ({
-          code: item.sku || "00000",
-          title: item.producto || "Producto sin nombre",
-          price: item.precio || 0,
-          qty: item.quantity,
-        })),
-      },
-    };
+  // const handlePayment = async () => {
+  //   const orderData = {
+  //     settings: {
+  //       endpoint: "https://pixel-pay.com",
+  //       credentials: {
+  //         username: "FHI372717363",
+  //         password: "b36aa20b0010f042b5bf788a4793b902",
+  //       },
+  //     },
+  //     card: {
+  //       number: "4111111111111111",
+  //       cvv2: "2512",
+  //       expire_month: 9,
+  //       expire_year: 2027,
+  //       cardholder: "Aerley Keller Lopez Ramos",
+  //     },
+  //     billing: {
+  //       address: "9 avenida",
+  //       country: "HN",
+  //       state: "HN-CR",
+  //       city: "San Pedro Sula",
+  //       phone: "98007330",
+  //     },
+  //     order: {
+  //       id: `ORDER-${Date.now()}`,
+  //       currency: "HNL",
+  //       customer_name: "Aerley Lopez",
+  //       customer_email: "example@gmail.com",
+  //       items: cart.map((item) => ({
+  //         code: item.sku || "00000",
+  //         title: item.producto || "Producto sin nombre",
+  //         price: item.precio || 0,
+  //         qty: item.quantity,
+  //       })),
+  //     },
+  //   };
 
-    try {
-      const response = await fetch("/api/transaction/sale", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(orderData),
-      });
+  //   try {
+  //     const response = await fetch("/api/transaction/sale", {
+  //       method: "POST",
+  //       headers: {
+  //         "Content-Type": "application/json",
+  //       },
+  //       body: JSON.stringify(orderData),
+  //     });
 
-      if (!response.ok) {
-        throw new Error(`Error al procesar la solicitud. Status: ${response.status}`);
-      }
+  //     if (!response.ok) {
+  //       throw new Error(`Error al procesar la solicitud. Status: ${response.status}`);
+  //     }
 
-      const result = await response.json();
-      console.log("Resultado de la respuesta:", result);
+  //     const result = await response.json();
+  //     console.log("Resultado de la respuesta:", result);
 
-      const isValidPayment = result.is_valid_payment;
-      if (isValidPayment) {
-        alert("Pago realizado con éxito");
-      } else {
-        alert("El pago no fue válido");
-      }
-    } catch (error) {
-      console.error("Error al realizar el pago:", error);
-      alert("Hubo un error al procesar el pago.");
-    }
-  };
+  //     const isValidPayment = result.is_valid_payment;
+  //     if (isValidPayment) {
+  //       alert("Pago realizado con éxito");
+  //     } else {
+  //       alert("El pago no fue válido");
+  //     }
+  //   } catch (error) {
+  //     console.error("Error al realizar el pago:", error);
+  //     alert("Hubo un error al procesar el pago.");
+  //   }
+  // };
 
   return (
     <>
