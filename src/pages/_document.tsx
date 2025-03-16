d49d888d1a6e342d9d7e5674a140aa61008a0f11
Aerley <tecpointdistribucion2@gmail.com>
2025-03-16 14:35:17 -0600

    actualizando la estructura del documento para mejorar SEO y agregar etiquetas de verificación de Google

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,16 +2,21 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="en">
-      {/* <Head /> */}
+    <Html lang="es">
       <Head>
+        {/* Google Site Verification */}
         <meta name="google-site-verification" content="rGu_PQAnMb87mm_8dS9oWQPpkuhg8eUwEuC8-3xKiDc" />
-        <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
-
-        <title>Distribuidores de Accesorios Tecnológicos | Tecpoint</title>
+        
+        {/* Favicon */}
         <link rel="shortcut icon" href="/favicon.png" type="image/x-icon" />
+
+        {/* SEO Meta Tags */}
+        <title>Distribuidores de Accesorios Tecnológicos | Tecpoint</title>
         <meta name="keywords" content="Distribuidor de accesorios tecnológicos en Honduras. Cargadores, adaptadores, audífonos, periféricos y más, al por mayor y al detalle." />
         <meta name="description" content="Distribuidor de accesorios tecnológicos en Honduras. Cargadores, adaptadores, audífonos, periféricos y más, al por mayor y al detalle." />
+        <meta name="robots" content="index, follow" />
+
+        {/* Open Graph (Facebook) */}
         <meta property="og:url" content="https://tecpoint.ws" />
         <meta property="og:image" content="https://firebasestorage.googleapis.com/v0/b/tecpoint-2024.appspot.com/o/logos%2Fog_image.png?alt=media&token=26d74138-1987-4143-86ce-31eab8af8338" />
         <meta property="og:title" content="Distribuidores de Accesorios Tecnológicos | Tecpoint" />
@@ -22,13 +27,25 @@ export default function Document() {
         <meta property="og:site_name" content="Tecpoint Distribucion - Honduras" />
         <meta property="og:locale" content="es_HN" />
 
+        {/* Twitter Meta Tags */}
         <meta property="twitter:title" content="Distribuidores de Accesorios Tecnológicos | Tecpoint" />
         <meta property="twitter:card" content="summary_large_image" />
         <meta property="twitter:description" content="Distribuidores de Accesorios Tecnológicos | Tecpoint" />
         <meta property="twitter:image" content="https://firebasestorage.googleapis.com/v0/b/tecpoint-2024.appspot.com/o/logos%2Fog_image.png?alt=media&token=26d74138-1987-4143-86ce-31eab8af8338" />
-
         <meta name="twitter:image:alt" content="Distribuidores de Accesorios Tecnológicos | Tecpoint" />
-        <meta name="robots" content="index, follow" />
+
+        {/* Google Ads Tag */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=AW-11071480891"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'AW-11071480891');
+            `,
+          }}
+        />
       </Head>
       <body>
         <Main />
